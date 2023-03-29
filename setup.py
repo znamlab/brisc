22991325adf3b7160062b48ff0e2583867c92d64
Antonin Blot <antonin.blot@crick.ac.uk>
2023-03-29 08:57:35 +0100

    Create package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="brisco",
+    packages=find_packages(),
+    install_requires=[
+        "cricksaw_analysis @ git+ssh://git@github.com/znamlab/cricksaw_analysis.git",
+        "flexiznam @ git+ssh://git@github.com/znamlab/flexiznam.git",
+    ],
+)
