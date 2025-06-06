c56c20581bac647b534a40637bd272518a201663
Antonin Blot <antonin.blot@crick.ac.uk>
2025-06-06 15:19:35 +0100

    [feature] adapt to different error corrections

--- brisc/manuscript_analysis/match_to_library.py
+++ brisc/manuscript_analysis/match_to_library.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import functools
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
@@ -15,47 +16,68 @@ matplotlib.rcParams[
 matplotlib.rcParams["ps.fonttype"] = 42  # For EPS, if relevant
 
 
+def _hamming_distance(str1, str2):
+    return sum(c1 != c2 for c1, c2 in zip(str1, str2))
+
+
+# Define a function to calculate the minimum edit distance
+def _calculate_min_edit_distance_worker(insitu_bc, lib_10bp_seq_ref, rv35_library_ref):
+    edit_distances = np.fromiter(
+        (_hamming_distance(insitu_bc, lib_bc) for lib_bc in lib_10bp_seq_ref), int
+    )
+    min_edit_distance_idx = np.argmin(edit_distances)
+    min_edit_distance = edit_distances[min_edit_distance_idx]
+    lib_bc_sequence = rv35_library_ref.loc[min_edit_distance_idx, "10bp_seq"]
+    lib_bc_count = rv35_library_ref.loc[min_edit_distance_idx, "counts"]
+    return min_edit_distance, lib_bc_sequence, lib_bc_count
+
+
 def load_data(
-        redo=False, 
-        barseq_path=Path("/nemo/project/proj-znamenp-barseq"), 
-        main_path=Path("/nemo/lab/znamenskiyp")
+    redo=False,
+    barseq_path=Path("/nemo/project/proj-znamenp-barseq"),
+    main_path=Path("/nemo/lab/znamenskiyp"),
+    error_correction_ds_name="BRAC8498.3e_error_corrected_barcodes_26",
 ):
     processed_path = barseq_path / "processed/becalia_rabies_barseq/BRAC8498.3e/"
     ara_is_starters = pd.read_pickle(
-        processed_path / "analysis" / "merged_cell_df_curated_mcherry.pkl"
+        processed_path / "analysis" / f"{error_correction_ds_name}_cell_barcode_df.pkl"
     )
+    #    ara_is_starters = pd.read_pickle(
+    #    processed_path / "analysis" / "merged_cell_df_curated_mcherry.pkl"
+    # )
     ara_is_starters = ara_is_starters[ara_is_starters["all_barcodes"].notna()]
-    in_situ_barcodes = ara_is_starters["all_barcodes"].explode().unique()
-    in_situ_barcodes = pd.DataFrame(in_situ_barcodes, columns=["sequence"])
 
-    barcode_library_sequence_path = main_path / "home/shared/projects/barcode_diversity_analysis/collapsed_barcodes/RV35/RV35_bowtie_ed2.txt"
+    barcode_library_sequence_path = (
+        main_path
+        / "home/shared/projects/barcode_diversity_analysis/collapsed_barcodes/RV35/RV35_bowtie_ed2.txt"
+    )
     rv35_library = pd.read_csv(barcode_library_sequence_path, sep="\t", header=None)
     rv35_library["10bp_seq"] = rv35_library[1].str.slice(0, 10)
     rv35_library.rename(columns={0: "counts", 1: "sequence"}, inplace=True)
 
-    def hamming_distance(str1, str2):
-        return sum(c1 != c2 for c1, c2 in zip(str1, str2))
-
-    # Define a function to calculate the minimum edit distance
-    def calculate_min_edit_distance(insitu_bc):
-        edit_distances = np.fromiter(
-            (hamming_distance(insitu_bc, lib_bc) for lib_bc in lib_10bp_seq), int
-        )
-        min_edit_distance_idx = np.argmin(edit_distances)
-        min_edit_distance = edit_distances[min_edit_distance_idx]
-        lib_bc_sequence = rv35_library.loc[min_edit_distance_idx, "10bp_seq"]
-        lib_bc_count = rv35_library.loc[min_edit_distance_idx, "counts"]
-        return min_edit_distance, lib_bc_sequence, lib_bc_count
-
     lib_10bp_seq = np.array(rv35_library["10bp_seq"])
-    if redo:
+    in_situ_barcodes_path = (
+        processed_path / "analysis" / f"{error_correction_ds_name}_in_situ_barcodes.pkl"
+    )
+    random_df_path = (
+        processed_path / "analysis" / f"{error_correction_ds_name}_random_barcodes.pkl"
+    )
+
+    if redo or not (in_situ_barcodes_path.exists() and random_df_path.exists()):
+        in_situ_barcodes = ara_is_starters["all_barcodes"].explode().unique()
+        in_situ_barcodes = pd.DataFrame(in_situ_barcodes, columns=["sequence"])
+
+        # Create a partial function with fixed library arguments
+        partial_worker = functools.partial(
+            _calculate_min_edit_distance_worker,
+            lib_10bp_seq_ref=lib_10bp_seq,
+            rv35_library_ref=rv35_library,
+        )
         # Wrap the outer loop with tqdm for progress tracking
         with Pool() as pool:
             results = list(
                 tqdm(
-                    pool.imap(
-                        calculate_min_edit_distance, in_situ_barcodes["sequence"]
-                    ),
+                    pool.imap(partial_worker, in_situ_barcodes["sequence"]),
                     total=len(in_situ_barcodes),
                     desc="Calculating edit distances",
                 )
@@ -86,7 +108,7 @@ def load_data(
         with Pool() as pool:
             results = list(
                 tqdm(
-                    pool.imap(calculate_min_edit_distance, random_sequences),
+                    pool.imap(partial_worker, random_sequences),
                     total=len(random_sequences),
                     desc="Calculating edit distances",
                 )
@@ -100,17 +122,11 @@ def load_data(
         random_df["min_edit_distance"] = min_edit_distances
         random_df["lib_bc_sequence"] = lib_bc_sequences
         random_df["lib_bc_counts"] = lib_bc_counts
-        random_df.to_pickle(
-            "/nemo/lab/znamenskiyp/home/users/becalia/data/BRYC65.1d/random_2252_barcodes.pkl"
-        )
-
+        random_df.to_pickle(random_df_path)
+        in_situ_barcodes.to_pickle(in_situ_barcodes_path)
     else:
-        in_situ_barcodes = pd.read_pickle(
-            main_path / "home/users/becalia/code/brisc/brisc/cells_and_barcodes/in_situ_barcodes.pkl"
-        )
-        random_df = pd.read_pickle(
-            main_path / "home/users/becalia/data/BRYC65.1d/random_2252_barcodes.pkl"
-        )
+        in_situ_barcodes = pd.read_pickle(in_situ_barcodes_path)
+        random_df = pd.read_pickle(random_df_path)
 
     return (
         in_situ_barcodes,
@@ -132,18 +148,26 @@ def plot_matches_to_library(
     # Define bin edges for consistent binning
     bin_edges = np.logspace(0, 6, num=num_bins)
     bin_edges = np.insert(bin_edges, 0, 0)
-    in_situ_barcode_matches.loc[in_situ_barcode_matches["ham_min_edit_distance"] > 0, "ham_lib_bc_counts"] = 0
-    random_barcode_matches.loc[random_barcode_matches["min_edit_distance"] > 0, "lib_bc_counts"] = 0
+    in_situ_barcode_matches.loc[
+        in_situ_barcode_matches["ham_min_edit_distance"] > 0, "ham_lib_bc_counts"
+    ] = 0
+    random_barcode_matches.loc[
+        random_barcode_matches["min_edit_distance"] > 0, "lib_bc_counts"
+    ] = 0
 
     # Compute histograms
-    in_situ_hist, _ = np.histogram(in_situ_barcode_matches["ham_lib_bc_counts"].values, bins=bin_edges)
+    in_situ_hist, _ = np.histogram(
+        in_situ_barcode_matches["ham_lib_bc_counts"].values, bins=bin_edges
+    )
     # hist1, _ = np.histogram(data1, bins=bin_edges)
-    random_hist, _ = np.histogram(random_barcode_matches["lib_bc_counts"].values, bins=bin_edges)
+    random_hist, _ = np.histogram(
+        random_barcode_matches["lib_bc_counts"].values, bins=bin_edges
+    )
 
     # Normalize histograms (scale max to 1)
     in_situ_hist = in_situ_hist / np.sum(in_situ_hist)
     # hist1 = hist1 / np.max(hist1)
-    random_hist = random_hist /  np.sum(random_hist)
+    random_hist = random_hist / np.sum(random_hist)
 
     # Extract and normalize library sequence data
     sequences = np.flip(rv35_library["counts"])
@@ -168,7 +192,9 @@ def plot_matches_to_library(
             label="In situ barcodes",
         )
         plt.stairs(
-            [in_situ_hist[0],],
+            [
+                in_situ_hist[0],
+            ],
             [0.03, 0.1],
             color="dodgerblue",
             linestyle="-",
@@ -183,12 +209,14 @@ def plot_matches_to_library(
             label="Random barcodes",
         )
         plt.stairs(
-            [random_hist[0],],
+            [
+                random_hist[0],
+            ],
             [0.03, 0.1],
             color="darkorange",
             linestyle="-",
             linewidth=line_width,
-        )    
+        )
 
         # Plot normalized library sequence data as a dashed black line
         plt.stairs(
@@ -209,7 +237,6 @@ def plot_matches_to_library(
             labelsize=tick_fontsize,
         )
 
-
     # X-axis formatting
     ax[0].set_xlabel(
         "Library barcode abundance",
@@ -217,7 +244,9 @@ def plot_matches_to_library(
     )
     # ax[0].xaxis.set_major_locator(mticker.FixedLocator(locs=np.logspace(0, 6, 5)))
     # ax[0].xaxis.set_minor_locator(mticker.LogLocator(numticks=999, subs="auto"))
-    ax[0].set_xticks([np.sqrt(0.03*0.1), 1, 1e3, 1e6], labels=["$0$", "$10^0$", "$10^3$", "$10^6$"])
+    ax[0].set_xticks(
+        [np.sqrt(0.03 * 0.1), 1, 1e3, 1e6], labels=["$0$", "$10^0$", "$10^3$", "$10^6$"]
+    )
 
     # Y-axis label
     ax[0].set_ylabel(
@@ -231,10 +260,4 @@ def plot_matches_to_library(
     # ax.set_ylim(0, 1.05)
     ax[1].spines.bottom.set_visible(False)
     ax[1].set_xticks([])
-    ax[1].legend(
-        loc="upper right",
-        fontsize=tick_fontsize,
-        frameon=False
-    )
-
-
+    ax[1].legend(loc="upper right", fontsize=tick_fontsize, frameon=False)
